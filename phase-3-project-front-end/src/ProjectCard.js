d0d16de463f94d0d1e4d1e1a0372d055df5a7d1a
Kaitlyn Greve <kgreve14@gmail.com>
2022-06-01 16:38:39 -0400

    more CRUD capabilities

--- phase-3-project-front-end/src/ProjectCard.js
+++ phase-3-project-front-end/src/ProjectCard.js
@@ -1,8 +1,29 @@
-function ProjectCard({ project }) {
+import { useState } from 'react'
+import ProjectEditForm from './ProjectEditForm'
+
+function ProjectCard({ project, updateProjectList }) {
+    const [isEdit, setIsEdit] = useState(false)
+    console.log(project.car_id)
+    const handleEditClick = () => {
+        setIsEdit(isEdit => !isEdit)
+    }
+
+    const handleDelete = (e) => {
+        fetch(`http://localhost:9292/projects/${project.id}`, {
+            method: 'DELETE'
+        })
+            .then(r => r.json())
+            .then(updateProjectList)
+    }
+
     return (
         <ul>
-            <li>{project.title}</li>
+            <li value={project}>{project.title}</li>
+            <button onClick={handleEditClick}>Edit Project</button>
+            {isEdit ? <ProjectEditForm project={project} /> : null}
+            <button onClick={handleDelete}>Delete Project</button>
         </ul>
+
     )
 }
 
