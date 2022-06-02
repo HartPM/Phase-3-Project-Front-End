e12b5a471660d2f77a2ccc37ea30a1a67b0bfb1e
Patrick Hart <pathart612@gmail.com>
2022-06-02 11:45:48 -0400

    core-deliverables-complete

--- phase-3-project-front-end/src/HomePage.js
+++ phase-3-project-front-end/src/HomePage.js
@@ -4,16 +4,7 @@ function HomePage() {
     return (
         <div>
             <h4>Welcome ... About ...</h4>
-            <div>
-                <button>Start a Project</button>
-                <button>View Your Garage</button>
-                <button>View Your Projects</button>
-            </div>
-            <div>
-                <img
-                    alt="wow cool photo"
-                    src="https://i.pinimg.com/originals/8f/6b/f7/8f6bf7288ef0cfe2ee071d3699aa90a2.jpg" />
-            </div>
+            <img src="https://i.pinimg.com/originals/8f/6b/f7/8f6bf7288ef0cfe2ee071d3699aa90a2.jpg" alt="wow cool" />
         </div>
     )
 }
